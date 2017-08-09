41590eaeb0dd566622ef11a492384fbd88b47617
Hunter <edward.hunter_1@philips.com>
2017-08-09 01:54:54 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,21 @@ $(document).ready(function() {
 	LetsBox();
 
 	function LetsBox() {
+		myAudio.play();
+		document.getElementById("myAudio").loop = true;
+		$("#pause").click(function() {
+			myAudio.pause();
+		})
+		$("#play").click(function() {
+			myAudio.play();
+		})
 		var index = 0;
 		var matchup = [];
 
-		var Boxers = [{Name:"Rocky Balboa", Health:150, AttackPower:7, CounterAttackPower:7, Player:false, Opponent:false}, 
-		{Name:"Apollo Creed", Health:150, AttackPower:8, CounterAttackPower:12, Player:false, Opponent:false},
-		{Name:"Clubber Lang", Health:140, AttackPower:10, CounterAttackPower:10, Player:false, Opponent:false},
-		{Name:"Ivan Drago", Health: 220, AttackPower: 10, CounterAttackPower:22, Player:false, Opponent:false}]
+		var Boxers = [{Name:"Rocky Balboa", Health:150, AttackPower:7, CounterAttackPower:7, AttckIncrease:7}, 
+		{Name:"Apollo Creed", Health:150, AttackPower:8, CounterAttackPower:9, AttckIncrease:8},
+		{Name:"Clubber Lang", Health:140, AttackPower:10, CounterAttackPower:11, AttckIncrease:10},
+		{Name:"Ivan Drago", Health: 220, AttackPower: 15, CounterAttackPower:22, AttckIncrease:15}]
 
 		var Rocky = Boxers[0];
 		var Apollo = Boxers[1];
@@ -20,14 +28,16 @@ $(document).ready(function() {
 
 			$(".col-md-12").append("<h1>ROCKY BOXING RPG!</h1>")
 
-			RB = $("<div><h4>Rocky Balboa</h4> <img src=assets/images/Rocky.jpg alt=Rock/><h3 id=RockyStats></h3></div>").attr("class","Rocky");
-			AC = $("<div><h4>Apollo Creed</h4> <img src=assets/images/Apollo.jpg alt=Creed/><h3 id=ApolloStats></h3></div>").attr("class","Apollo");
-			CL = $("<div><h4>Clubber Lang</h4> <img src=assets/images/Clubber.jpg alt=Lang/><h3 id=ClubberStats></h3><h3 id=stats></h3></div>").attr("class","Clubber");
-			ID = $("<div><h4>Ivan Drago</h4> <img src=assets/images/Ivan.jpg alt=Drago/><h3 id=IvanStats></h3></div>").attr("class","Ivan");
-			VersusBox = $("<div></div>").attr("id","VersusBox", "class", "col-md-4");
+			RB = $("<div><h3>Rocky Balboa</h3> <img src=assets/images/Rocky.jpg alt=Rock/><h3 id=RockyStats></h3></div>").attr("class","Rocky");
+			AC = $("<div><h3>Apollo Creed</h3> <img src=assets/images/Apollo.jpg alt=Creed/><h3 id=ApolloStats></h3></div>").attr("class","Apollo");
+			CL = $("<div><h3>Clubber Lang</h3> <img src=assets/images/Clubber.jpg alt=Lang/><h3 id=ClubberStats></h3><h3 id=stats></h3></div>").attr("class","Clubber");
+			ID = $("<div><h3>Ivan Drago</h3> <img src=assets/images/Ivan.jpg alt=Drago/><h3 id=IvanStats></h3></div>").attr("class","Ivan");
+			VersusBox = $("<div></div>").attr("id","VersusBox");
 
 			$("#fighterRow").append(RB, AC, CL, ID, "<h1 id=chooseFighter>Choose Your Fighter!</h1>");
-			$("#VersusRow").append(VersusBox);
+			// $("#VersusRow").append(VersusBox);
+			$("#VersusRow").append("<div id=characterBox></div>")
+			$("#VersusRow").append("<div id=opponentBox></div>")
 			$("#AttackRow").append("<button id=Attackbtn>FIGHT!</button>");
 
 			$("#RockyStats").append("Health: " + Rocky.Health);
@@ -42,97 +52,105 @@ $(document).ready(function() {
 			})
 
 			RB.one('click', function() {
-				RB.data('clicked', true);
-				// RB.animate({"top": "+=428px"});
-				// RB.animate({"right": "-=253px"})
-				if (matchup[0] === undefined || matchup[1] === undefined) {
-					VersusBox.append(RB);
+				if (matchup[0] === undefined) {
+					$("#characterBox").append("<h3>Your Character</h3>");
+					$("#characterBox").prepend(RB);
+					matchup[0] = Rocky;
 					index += 1;
 				}
-				if (index === 1) {
-					Rocky.Player = true;
-					matchup[0] = Rocky;
-				}
-				else if (index === 2 && matchup[1] === undefined) {
-					$("#VersusBox").prepend("<h2>VERSUS!</h2>");
-					Rocky.Opponent = true;
+				else if (index === 1 && matchup[1] === undefined) {
+					$("#VersusBox").append("<h2>VERSUS!</h2>");
+					$("#opponentBox").append("<h3>Your Opponent</h3>");
+					$("#opponentBox").prepend(RB);
 					matchup[1] = Rocky;
+					index += 1
 				}
-				else if (index >= 3) {
-					Rocky.Opponent = true;
+				else if (index === 2) {
+					$("#opponentBox").prepend(RB);
+					matchup[1] = Rocky;
+					index += 1;
+				}
+				else if (index === 3) {
+					$("#opponentBox").prepend(RB);
 					matchup[1] = Rocky;
 				}
 			});
 
 			AC.one('click', function() {
-				AC.data('clicked', true);
-				// AC.animate({"top": "+=428px"});
-				// AC.animate({"right": "-=2.5px"});
-				if (matchup[0] === undefined || matchup[1] === undefined) {
-					VersusBox.append(AC);
+				if (matchup[0] === undefined) {
+					$("#characterBox").append("<h3>Your Character</h3>");
+					$("#characterBox").prepend(AC);
+					matchup[0] = Apollo;
 					index += 1;
 				}
-				if (index === 1) {
-					Apollo.Player = true;
-					matchup[0] = Apollo;
-				}
-				else if (index === 2 && matchup[1] === undefined) {
-					$("#VersusBox").prepend("<h2>VERSUS!</h2>");
-					Apollo.Opponent = true;
+				else if (index === 1 && matchup[1] === undefined) {
+					$("#VersusBox").append("<h2>VERSUS!</h2>");
+					$("#opponentBox").append("<h3>Your Opponent</h3>");
+					$("#opponentBox").prepend(AC);
 					matchup[1] = Apollo;
+					index += 1
 				}
-				else if (index >= 3) {
-					Apollo.Opponent = true;
+				else if (index === 2) {
+					$("#opponentBox").prepend(AC);
+					matchup[1] = Apollo;
+					index += 1;
+				}
+				else if (index === 3) {
+					$("#opponentBox").prepend(AC);
 					matchup[1] = Apollo;
 				}
 			});
 
 			CL.one('click', function() {
-				CL.data('clicked', true);
-				// CL.animate({"top": "+=428px"});
-				// CL.animate({"right": "+=2.5px"})
-				if (matchup[0] === undefined || matchup[1] === undefined) {
-					VersusBox.append(CL);
+				if (matchup[0] === undefined) {
+					$("#characterBox").append("<h3>Your Character</h3>");
+					$("#characterBox").prepend(CL);
+					matchup[0] = Clubber;
 					index += 1;
 				}
-				if (index === 1) {
-					Clubber.Player = true;
-					matchup[0] = Clubber;
-				}
-				else if (index === 2 && matchup[1] === undefined) {
-					$("#VersusBox").prepend("<h2>VERSUS!</h2>");
-					Clubber.Opponent = true;
+				else if (index === 1 && matchup[1] === undefined) {
+					$("#VersusBox").append("<h2>VERSUS!</h2>");
+					$("#opponentBox").append("<h3>Your Opponent</h3>");
+					$("#opponentBox").prepend(CL);
 					matchup[1] = Clubber;
+					index += 1
 				}
-				else if (index >= 3) {
-					Clubber.Opponent = true;
+				else if (index === 2) {
+					$("#opponentBox").prepend(CL);
+					matchup[1] = Clubber;
+					index += 1;
+				}
+				else if (index === 3) {
+					$("#opponentBox").prepend(CL);
 					matchup[1] = Clubber;
 				}
 			});
 
 			ID.one('click', function() {
-				ID.data('clicked', true);
-				// ID.animate({"top": "+=428px"});
-				// ID.animate({"right": "+=253.5px"})
-				if (matchup[0] === undefined || matchup[1] === undefined) {
-					VersusBox.append(ID);
+				if (matchup[0] === undefined) {
+					$("#characterBox").append("<h3>Your Character</h3>");
+					$("#characterBox").prepend(ID);
+					matchup[0] = Ivan;
 					index += 1;
 				}
-				if (index === 1) {
-					Ivan.Player = true;
-					matchup [0] = Ivan;
-				}
-				else if (index === 2 && matchup[1] === undefined) {
-					$("#VersusBox").prepend("<h2>VERSUS!</h2>");
-					Ivan.Opponent = true;
+				else if (index === 1 && matchup[1] === undefined) {
+					$("#VersusBox").append("<h2>VERSUS!</h2>");
+					$("#opponentBox").append("<h3>Your Opponent</h3>");
+					$("#opponentBox").prepend(ID);
 					matchup[1] = Ivan;
+					index += 1
 				}
-				else if (index >= 3) {
-					Ivan.Opponent = true;
+				else if (index === 2) {
+					$("#opponentBox").prepend(ID);
 					matchup[1] = Ivan;
-
+					index += 1;
+				}
+				else if (index === 3) {
+					$("#opponentBox").prepend(ID);
+					matchup[1] = Ivan;
 				}
 			});
+
 			$("#Attackbtn").click(function() {
 				if (matchup[0] === undefined) {
 					return alert("Choose Your Fighter!");
@@ -145,7 +163,7 @@ $(document).ready(function() {
 					matchup[0].Health -= matchup[1].CounterAttackPower;
 					$("#FighterDmgReport").html("You Punched " + matchup[1].Name + " For " + matchup[0].AttackPower + " Damage!");
 					$("#OpponentDmgReport").html(matchup[1].Name + " Counterpunched " + matchup[0].Name + " For " + matchup[1].CounterAttackPower + " Damage!")
-					matchup[0].AttackPower += 6;
+					matchup[0].AttackPower += matchup[0].AttckIncrease;
 
 					if (matchup[0].Name === "Rocky Balboa") {
 						if (matchup[0].Health <= 0) {
